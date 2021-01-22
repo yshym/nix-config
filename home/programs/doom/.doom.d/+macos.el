445201507a70f13e17dd02e0ff24d878dd2665ca
Yevhen Shymotiuk <yevhenshymotiuk@pm.me>
2021-01-23 00:53:57 +0200

    Update doom macos config

--- home/programs/doom/.doom.d/+macos.el
+++ home/programs/doom/.doom.d/+macos.el
@@ -1,2 +1,14 @@
-(load! "+animations")
+;; (load! "+animations")
 
+;; disable option
+(setq mac-option-modifier nil)
+
+;; increase fringe to see diff
+;; while using window border
+(setq +vc-gutter-in-margin t)
+
+(defun set-fringe ()
+  "Set fringe."
+  (fringe-mode '(15 . 10)))
+
+(add-hook 'prog-mode-hook 'set-fringe)
