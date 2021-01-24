de017922c2dc71716b9a3ed72ec5310e84a6edd9
Yevhen Shymotiuk <yevhenshymotiuk@pm.me>
2021-01-24 22:39:52 +0200

    Update black.sh script

--- home/programs/scripts/bin/black.sh
+++ home/programs/scripts/bin/black.sh
@@ -1,5 +1,14 @@
 project_root="$1"
-config_path="$(find "$project_root" -name "pyproject.toml" || echo \"$HOME/.config/black/pyproject.toml\")"
+
+system_config_path="$HOME/.config/black/pyproject.toml"
+project_config_path="$(find "$project_root" -name "pyproject.toml")"
+project_config_contains_black_code="$(grep "\[tool\.black\]" < "$project_config_path" 1> /dev/null && echo "$?")"
+
+config_path="$([ "$project_config_path" != "" ] && \
+    [ "$project_config_contains_black_code" = 0 ] && \
+    echo "$project_config_path" || \
+    echo "$system_config_path")"
+
 echo "Using '$config_path'"
 
 black --config="$config_path" "$2"
