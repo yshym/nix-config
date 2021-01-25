323c506861a4b17e1f896534883e5799a28f5e96
Yevhen Shymotiuk <yevhenshymotiuk@pm.me>
2021-01-25 02:42:22 +0200

    Update keyboardLayout hammerspoon image

--- home/programs/hammerspoon/.hammerspoon/keyboardLayout/init.lua
+++ home/programs/hammerspoon/.hammerspoon/keyboardLayout/init.lua
@@ -3,6 +3,12 @@ keyboardLayout = {}
 function keyboardLayout:init()
   self:draw()
   self:setLayoutChangedHook()
+
+  local function nextLayout()
+    self:setNextLayout()
+  end
+
+  hs.hotkey.bind({"ctrl", "alt"}, "space", nextLayout)
 end
 
 function keyboardLayout:currentLayoutTextElement()
@@ -18,29 +24,53 @@ function keyboardLayout:currentLayoutTextElement()
     }
   )
 
-  return { type = "text", id = "istext", text = text, action = "fill" }
+  return { type = "text", id = "kltext", text = text, action = "fill" }
+end
+
+function keyboardLayout:currentLayoutImageElement()
+  kc = hs.keycodes
+  text = kc.currentLayout()
+
+  image = text == "ABC"
+    and hs.image.imageFromPath("~/.hammerspoon/keyboardLayout/uk.png")
+    or kc.currentLayoutIcon()
+
+  return { type = "image", id = "klimage", image = image }
 end
 
 function keyboardLayout:draw()
   canvas = hs.canvas
-  self.c = canvas.new{ x = 1539, y = 0, h = 30, w = 64 }:show()
+  self.c = canvas.new{ x = 1550, y = 6.5, h = 18, w = 64 }:show()
   self.c:behavior("canJoinAllSpaces")
   self.c:level("normal")
   self.c:insertElement({
     type = "rectangle",
-    id = "isrect",
+    id = "klrect",
     action = "fill",
     fillColor = { hex = "202020", alpha = 0 }
   })
-  self.c:insertElement(self:currentLayoutTextElement())
+  self.c:insertElement(self:currentLayoutImageElement())
 end
 
 function keyboardLayout:setLayoutChangedHook()
   local function updateLayout()
     self.c:removeElement(2)
-    self.c:insertElement(self:currentLayoutTextElement())
+    self.c:insertElement(self:currentLayoutImageElement())
   end
   hs.keycodes.inputSourceChanged(updateLayout)
+end
+
+function keyboardLayout:setNextLayout()
+  kc = hs.keycodes
+  layouts = kc.layouts()
+
+  for i, name in ipairs(layouts) do
+    if name == kc.currentLayout() then
+      nextIndex = i == #layouts and 1 or i + 1
+    end
+  end
+
+  kc.setLayout(layouts[nextIndex])
 end
 
 return keyboardLayout
--- home/programs/hammerspoon/default.nix
+++ home/programs/hammerspoon/default.nix
@@ -3,6 +3,7 @@
 {
   home = {
     file.".hammerspoon/init.lua".source = ./.hammerspoon/init.lua;
+    file.".hammerspoon/stackline".source = ./.hammerspoon/stackline;
     file.".hammerspoon/keyboardLayout".source = ./.hammerspoon/keyboardLayout;
   };
 }
