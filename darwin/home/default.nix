1f1b19671431a50970c1c4396d3415b412e58649
Yevhen Shymotiuk <yevhenshymotiuk@pm.me>
2021-05-20 18:59:03 +0300

    Update darwin apps aliasing

--- darwin/home/default.nix
+++ darwin/home/default.nix
@@ -25,11 +25,9 @@ in {
     echo "setting up ~/Applications/Nix..."
     rm -rf ~/Applications/Nix
     mkdir -p ~/Applications/Nix
-    chown ${me} ~/Applications/Nix
-    find ${config.system.build.applications}/Applications -maxdepth 1 -type l | while read f; do
-      src="$(/usr/bin/stat -f%Y $f)"
-      appname="$(basename $src)"
-      osascript -e "tell app \"Finder\" to make alias file at POSIX file \"/Users/${me}/Applications/Nix/\" to POSIX file \"$src\" with properties {name: \"$appname\"}";
+    for app in $(find ${config.system.build.applications}/Applications -maxdepth 1 -type l); do
+      src="$(/usr/bin/stat -f%Y "$app")"
+      cp -r "$src" ~/Applications/Nix
     done
 
     # create password store symlink
