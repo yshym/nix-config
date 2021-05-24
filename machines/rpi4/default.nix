e36b61580318988f1d5f6c55f488fe31a81c2873
Yevhen Shymotiuk <yevhenshymotiuk@pm.me>
2021-05-24 18:45:20 +0300

    Remove networking interfaces configuration from rpi4 machine

--- machines/rpi4/default.nix
+++ machines/rpi4/default.nix
@@ -20,16 +20,6 @@
 
   networking = {
     hostName = "rpi4";
-    interfaces = {
-      wlan0 = {
-        ipv4 = {
-          addresses = [{
-            address = "192.168.0.105";
-            prefixLength = 24;
-          }];
-        };
-      };
-    };
   };
 
   programs = {
