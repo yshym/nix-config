b3af991a881c8f81c04c24b1583e4c25bce7e12c
Yevhen Shymotiuk <yevhenshymotiuk@pm.me>
2021-05-27 22:45:20 +0300

    Use postgresql 12

--- machines/mbp16/default.nix
+++ machines/mbp16/default.nix
@@ -4,4 +4,11 @@
   imports = [ ../../platforms/darwin ];
 
   networking.hostName = "mbp16";
+
+  services = {
+    postgresql = {
+      enable = true;
+      package = pkgs.postgresql_12;
+    };
+  };
 }
