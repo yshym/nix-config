492db06bc786c95200b1337be4dfa4dd5ac2c75e
Yevhen Shymotiuk <yevhenshymotiuk@pm.me>
2021-05-29 01:37:28 +0300

    Get rid of prezto and oh-my-zsh

--- home/programs/zsh/default.nix
+++ home/programs/zsh/default.nix
@@ -3,7 +3,7 @@
 {
   # imports = [ ./forgit.nix ];
 
-  home.packages = with pkgs; [ zsh-completions ];
+  home.packages = with pkgs; [ zsh-completions zsh-powerlevel10k ];
 
   home.file.".zsh_completions".source = ./completions;
   home.file.".p10k-pure.zsh".source = ./p10k-pure.zsh;
@@ -16,19 +16,20 @@
       ssh-add $HOME/.ssh/id_ed25519 &> /dev/null
     '';
     initExtra = ''
-      export HISTSIZE=1000
-      export SAVEHIST=1000
-
+      source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
       source ~/.p10k-pure.zsh
-      if command -v pyenv 1> /dev/null 2>&1; then
-        eval "$(pyenv init -)"
-      fi
+
+      bindkey -M emacs '^K' history-substring-search-up
+      bindkey '^[[A' history-substring-search-up
+      bindkey -M emacs '^J' history-substring-search-down
+      bindkey '^[[B' history-substring-search-down
     '';
     envExtra = ''
       fpath+=$HOME/.zsh_completions
 
       export ERL_AFLAGS="-kernel shell_history enabled"
       export PYENV_ROOT="$HOME/.pyenv"
+
       export GOPATH="$HOME/go"
       # export DARTPATH=$(dirname $(dirname $(readlink $(which dart))))
 
@@ -37,34 +38,36 @@
       export PATH="$GOPATH/bin:$PATH"
       export PATH="$HOME/.local/bin:$PATH"
     '';
-    oh-my-zsh = {
-      enable = true;
-      plugins = [ "docker" "docker-compose" "wd" "z" ];
-    };
-    prezto = {
-      enable = true;
-      pmodules = [
-        "editor"
-        "spectrum"
-        "utility"
-        "completion"
-        "history-substring-search"
-        "docker"
-        "git"
-        "prompt"
-      ];
-      editor.keymap = "emacs";
-      prompt.theme = "powerlevel10k";
-    };
-    plugins = with pkgs; [rec {
-      name = "fast-syntax-highlighting";
-      src = fetchFromGitHub {
-        owner = "zdharma";
-        repo = name;
-        rev = "5351bd907ea39d9000b7bd60b5bb5b0b1d5c8046";
-        sha256 = "0h7f27gz586xxw7cc0wyiv3bx0x3qih2wwh05ad85bh2h834ar8d";
-      };
-    }];
+    history.size = 1000;
+    plugins = with pkgs; [
+      rec {
+        name = "fast-syntax-highlighting";
+        src = fetchFromGitHub {
+          owner = "zdharma";
+          repo = name;
+          rev = "v1.55";
+          sha256 = "0h7f27gz586xxw7cc0wyiv3bx0x3qih2wwh05ad85bh2h834ar8d";
+        };
+      }
+      rec {
+        name = "zsh-history-substring-search";
+        src = fetchFromGitHub {
+          owner = "zsh-users";
+          repo = name;
+          rev = "v1.0.2";
+          sha256 = "0y8va5kc2ram38hbk2cibkk64ffrabfv1sh4xm7pjspsba9n5p1y";
+        };
+      }
+      rec {
+        name = "zsh-z";
+        src = fetchFromGitHub {
+          owner = "agkozak";
+          repo = name;
+          rev = "595c883abec4682929ffe05eb2d088dd18e97557";
+          sha256 = "sha256-HnwUWqzwavh/Qox+siOe5lwTp7PBdiYx+9M0NMNFx00=";
+        };
+      }
+    ];
     shellAliases = {
       cat = "bat";
       cdr = "cd $(git rev-parse --show-toplevel)";
