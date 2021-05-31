7457032152e6958457fa046ef45a34ac70f94ac7
Yevhen Shymotiuk <yevhenshymotiuk@pm.me>
2021-05-31 18:56:56 +0300

    Setup NIX_PATH in zsh

--- home/programs/zsh/default.nix
+++ home/programs/zsh/default.nix
@@ -41,6 +41,9 @@ with pkgs; {
       bindkey "^J"   history-substring-search-down # ctrl-j
       bindkey "^[[A" history-substring-search-up   # up arrow
       bindkey "^[[B" history-substring-search-down # down arrow
+
+      export NIX_PATH="/nix/var/nix/profiles/per-user/root/channels:$HOME/.nix-defexpr/channels"
+      ${lib.optionalString stdenv.isDarwin "export NIX_PATH=\"darwin-config=$HOME/.nixpkgs/configuration.nix:$NIX_PATH\""}
     '';
     envExtra = ''
       fpath+=$HOME/.zsh_completions
