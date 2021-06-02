0174173a92aa53f7e1d8baa6b21e4ffcf8d2717e
Yevhen Shymotiuk <yevhenshymotiuk@pm.me>
2021-06-02 16:34:04 +0300

    Add yevhenshymotiuk cachix cache

---
+++ cachix/yevhenshymotiuk.nix
@@ -0,0 +1,10 @@
+{
+  nix = {
+    binaryCaches = [
+      "https://yevhenshymotiuk.cachix.org"
+    ];
+    binaryCachePublicKeys = [
+      "yevhenshymotiuk.cachix.org-1:N+ywirO93JL/oUQ9vSfKVQhDUSq1jZjFQtq1oK4HTaw="
+    ];
+  };
+}
