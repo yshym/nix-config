fc875c887ee3886568cdab613f2390f299238a20
Yevhen Shuymotiuk <yevhenshymotiuk@pm.me>
2021-06-13 10:34:06 +0000

    spotifyd: use pulseaudio for backend

--- machines/rpi4/home.nix
+++ machines/rpi4/home.nix
@@ -12,15 +12,13 @@
             username = "31ar6mmjcalpg3e76aifbrzs55bi";
             password_cmd = "pass Spotify/31ar6mmjcalpg3e76aifbrzs55bi";
             use_keyring = true;
-            backend = "alsa";
-            device = "default";
-            control = "default";
+            backend = "pulseaudio";
             mixer = "PCM";
             volume_controller = "softvol";
             bitrate = 320;
             device_name = "rpi4";
             device_type = "speaker";
-            initial_volume = "70";
+            initial_volume = "50";
             volume_normalization = false;
           };
         };
