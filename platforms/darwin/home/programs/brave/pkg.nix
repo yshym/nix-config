0f36b15e857eb72e9471edf66b193505005a7d72
Yevhen Shymotiuk <yevhenshymotiuk@pm.me>
2021-07-31 20:26:41 +0300

    darwin: update brave to 1.26.74

--- platforms/darwin/home/programs/brave/pkg.nix
+++ platforms/darwin/home/programs/brave/pkg.nix
@@ -2,21 +2,21 @@
 
 stdenv.mkDerivation rec {
   pname = "Brave";
-  version = "1.25.68";
+  version = "1.26.74";
 
   buildInputs = [ undmg ];
   sourceRoot = ".";
   phases = [ "unpackPhase" "installPhase" ];
   installPhase = ''
     mkdir -p "$out/Applications"
-    cp -r Brave\ Browser.app "$out/Applications/Brave.app"
+    cp -r Brave\ Browser.app $out/Applications/Brave\ Browser.app
   '';
 
   src = fetchurl {
     name = "Brave-${version}.dmg";
     url =
       "https://github.com/brave/brave-browser/releases/download/v${version}/Brave-Browser-x64.dmg";
-    sha256 = "0vj6i76lp290qvvj0j8k0fgz34aqw46yp9rhb65wzakafgswpgix";
+    sha256 = "18crc2m6b1hp3fc86g4n78f8xx76cpqmgdfggvd1s4hhmln8qbvi";
   };
 
   meta = with lib; {
--- platforms/darwin/home/programs/default.nix
+++ platforms/darwin/home/programs/default.nix
@@ -2,6 +2,7 @@
 
 {
   imports = [
+    # ./brave
     ./emacs
     ./firefox
     ./hammerspoon
