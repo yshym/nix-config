0c0911ab8792c830cdc7f7e3ab290977e570a41a
Yevhen Shymotiuk <yevhenshymotiuk@pm.me>
2021-09-20 19:37:05 +0300

    darwin: enable eshell in doom config

--- platforms/darwin/home/programs/emacs/.doom.d/+bindings.el
+++ platforms/darwin/home/programs/emacs/.doom.d/+bindings.el
@@ -3,22 +3,35 @@
 
 ;; web
 (map! (:after web-mode
-        (:map web-mode-map
-          "TAB" nil
-          "TAB" 'emmet-expand-yas)))
+       (:map web-mode-map
+        "TAB" nil
+        "TAB" 'emmet-expand-yas)))
 
 
 ;; company
 (map! (:after company
-        (:map company-active-map
-          "<tab>" nil
-          "TAB" 'company-complete-selection)))
+       (:map company-active-map
+        "<tab>" nil
+        "TAB" 'company-complete-selection)))
 
 
 ;; verb
 (map! (:after verb
        (:map verb-mode-map
-          "C-c v" 'verb-send-request-on-point)))
+        "C-c v" 'verb-send-request-on-point)))
+
+
+(defun eshell/clear-to-top ()
+  "Clear contents of eshell window and move to top."
+  (interactive)
+  (let ((inhibit-read-only t))
+    (erase-buffer)
+    (eshell-send-input)))
+
+
+(map! (:after eshell
+       (:map eshell-mode-map
+        "C-l" 'eshell/clear-to-top)))
 
 
 ;; <leader>
