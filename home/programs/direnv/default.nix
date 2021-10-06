7d5858b3add94ef3f78b0932122faea834508a86
Yevhen Shymotiuk <yevhenshymotiuk@pm.me>
2021-10-07 00:20:52 +0300

    direnv: enable nix-direnv and refactor config

--- home/programs/direnv/default.nix
+++ home/programs/direnv/default.nix
@@ -1,18 +1,36 @@
 { config, lib, pkgs, ... }:
 
 with lib;
+with pkgs;
 let cfg = config.programs.direnv;
 in {
   programs.direnv = {
     enable = true;
     enableZshIntegration = false;
+    nix-direnv = {
+      enable = true;
+      enableFlakes = true;
+    };
     config = {
       whitelist = {
         prefix = [ "${config.home.homeDirectory}/dev" ];
         exact = [ "/etc/nixos" ];
       };
     };
-  };
+    stdlib = ''
+      layout_poetry() {
+        if [[ ! -f pyproject.toml ]]; then
+          log_error 'No pyproject.toml found. Use `poetry new` or `poetry init` to create one first.'
+          exit 2
+        fi
 
-  home = mkIf cfg.enable { file.".direnvrc".source = ./direnvrc; };
+        # create venv if it doesn't exist
+        poetry run true
+
+        export VIRTUAL_ENV=$(poetry env info --path)
+        export POETRY_ACTIVE=1
+        PATH_add "$VIRTUAL_ENV/bin"
+      }
+    '';
+  };
 }
