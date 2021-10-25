fea5b0ec5e078fd238797fae7c9e6c28411ce454
Yevhen Shymotiuk <yevhenshymotiuk@pm.me>
2021-10-26 00:33:06 +0300

    imap: remove unused files and services

--- platforms/darwin/home/programs/scripts/bin/offlineimap.sh
+++
@@ -1,6 +0,0 @@
-offlineimap -a Personal & pid1=$!
-# offlineimap -a Work & pid2=$!
-
-wait $pid1
-# wait $pid2
-echo "Last execution: $(date)"
--- platforms/darwin/services/imap.nix
+++ platforms/darwin/services/imap.nix
@@ -8,15 +8,15 @@ let
     "${home.homeDirectory}/.config/imapnotify/gmail/notify.conf";
 in {
   launchd.user.agents = {
-    goimapnotify.serviceConfig = {
-      ProgramArguments =
-        [ "${pkgs.goimapnotify}/bin/goimapnotify" "-conf" imapnotifyConfig ];
-      UserName = "${me}";
-      RunAtLoad = true;
-      ThrottleInterval = 30;
-      EnvironmentVariables.PATH =
-        "${homeManagerPath}/bin:${config.environment.systemPath}";
-    };
+    # goimapnotify.serviceConfig = {
+    #   ProgramArguments =
+    #     [ "${pkgs.goimapnotify}/bin/goimapnotify" "-conf" imapnotifyConfig ];
+    #   UserName = "${me}";
+    #   RunAtLoad = true;
+    #   ThrottleInterval = 30;
+    #   EnvironmentVariables.PATH =
+    #     "${homeManagerPath}/bin:${config.environment.systemPath}";
+    # };
     mbsync.serviceConfig = {
       ProgramArguments = [ "${pkgs.isync}/bin/mbsync" "-a" ];
       UserName = "${me}";
