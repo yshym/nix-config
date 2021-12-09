a36b945c49d4e3bead658aee3c0588b6e7b19bc3
Yevhen Shymotiuk <yevhenshymotiuk@pm.me>
2021-12-09 19:54:23 +0200

    fl: update sway config

--- machines/fl/home/wayland/sway/default.nix
+++ machines/fl/home/wayland/sway/default.nix
@@ -2,217 +2,234 @@
 
 let
   cfg = config.wayland.windowManager.sway;
-  blue = "#3366CC";
+  darkBlue = "#6272a4";
 in {
-  config.wayland.windowManager.sway = {
-    enable = true;
-    systemdIntegration = true;
-    xwayland = true;
-    extraSessionCommands = ''
-      export SDL_VIDEODRIVER=wayland
-      # needs qt5.qtwayland in systemPackages
-      export QT_QPA_PLATFORM=wayland
-      export QT_WAYLAND_DISABLE_WINDOWDECORATION="1"
-      # fix for some Java AWT applications (e.g. Android Studio),
-      # use this if they aren't displayed properly:
-      export _JAVA_AWT_WM_NONREPARENTING=1
-    '';
-    config = {
-      fonts = {
-        names = [ "Fira Code" ];
-        size = 15.0;
-      };
-
-      window = {
-        titlebar = false;
-        border = 3;
-      };
-
-      focus = { followMouse = true; };
-
-      # TODO: Set up application to workspace assignments
-      # assigns = {};
-
-      modifier = "Mod4";
-
-      colors = {
-        focused = {
-          border = "#353439";
-          background = blue;
-          text = "#7B9F35";
-          indicator = blue;
-          childBorder = blue;
+  config = {
+    wayland.windowManager.sway = {
+      enable = true;
+      systemdIntegration = true;
+      xwayland = true;
+      extraSessionCommands = ''
+        export SDL_VIDEODRIVER=wayland
+        # needs qt5.qtwayland in systemPackages
+        export QT_QPA_PLATFORM=wayland
+        export QT_WAYLAND_DISABLE_WINDOWDECORATION="1"
+        # fix for some Java AWT applications (e.g. Android Studio),
+        # use this if they aren't displayed properly:
+        export _JAVA_AWT_WM_NONREPARENTING=1
+      '';
+      config = {
+        fonts = {
+          names = [ "Fira Code" ];
+          size = 15.0;
         };
-      };
 
-      bars = [ ];
-
-      # TODO: Set up startup commands
-      # startup = {};
-
-      gaps = {
-        inner = 5;
-        outer = 3;
-        smartGaps = true;
-      };
-
-      menu = "rofi -show drun";
-
-      terminal = "alacritty";
-
-      input = {
-        "1:1:AT_Translated_Set_2_keyboard" = {
-          xkb_layout = "us,ru,ua";
-          xkb_options = "grp:alt_shift_toggle";
+        window = {
+          titlebar = false;
+          border = 3;
         };
-        "2362:628:PIXA3854:00_093A:0274_Touchpad" = {
-          dwt = "enabled";
-          natural_scroll = "enabled";
-          tap = "enabled";
-          tap_button_map = "lrm";
-          pointer_accel = "0.2";
+
+        focus = { followMouse = true; };
+
+        # TODO: Set up application to workspace assignments
+        # assigns = {};
+
+        modifier = "Mod4";
+
+        colors = {
+          focused = {
+            border = "#353439";
+            background = darkBlue;
+            text = "#7B9F35";
+            indicator = darkBlue;
+            childBorder = darkBlue;
+          };
         };
-      };
 
-      # TODO: Set up outputs
-      # output = { "<display-name>" = { bg = "<path-to-background> stretch"; }; };
+        bars = [ ];
 
-      seat = {
-        "2362:628:PIXA3854:00_093A:0274_Touchpad" = { hide_cursor = "3000"; };
-      };
+        # TODO: Set up startup commands
+        # startup = {};
 
-      keybindings = {
-        # start a terminal
-        "${cfg.config.modifier}+Return" = "exec ${cfg.config.terminal}";
+        gaps = {
+          inner = 5;
+          outer = 3;
+          smartGaps = true;
+        };
 
-        # kill focused window
-        "${cfg.config.modifier}+Shift+q" = "kill";
+        menu = "rofi -show drun";
 
-        # start a menu app
-        "${cfg.config.modifier}+d" = "exec ${cfg.config.menu}";
+        terminal = "alacritty";
 
-        # make a screenshot
-        "${cfg.config.modifier}+Print" = "exec ~/.local/bin/maim.sh";
-        "${cfg.config.modifier}+Shift+Print" = "exec ~/.local/bin/maim.sh -s";
+        input = {
+          "1:1:AT_Translated_Set_2_keyboard" = {
+            xkb_layout = "us,ru,ua";
+            xkb_options = "grp:alt_shift_toggle";
+          };
+          "2362:628:PIXA3854:00_093A:0274_Touchpad" = {
+            dwt = "enabled";
+            natural_scroll = "enabled";
+            tap = "enabled";
+            tap_button_map = "lrm";
+            pointer_accel = "0.1";
+          };
+        };
 
-        # change focus
-        "${cfg.config.modifier}+${cfg.config.left}" = "focus left";
-        "${cfg.config.modifier}+${cfg.config.down}" = "focus down";
-        "${cfg.config.modifier}+${cfg.config.up}" = "focus up";
-        "${cfg.config.modifier}+${cfg.config.right}" = "focus right";
-        "${cfg.config.modifier}+Left" = "focus left";
-        "${cfg.config.modifier}+Down" = "focus down";
-        "${cfg.config.modifier}+Up" = "focus up";
-        "${cfg.config.modifier}+Right" = "focus right";
+        # TODO: Set up outputs
+        output = {
+          "eDP-1" = { bg = "~/.local/share/wallpaper.png stretch"; };
+        };
 
-        # move focused window
-        "${cfg.config.modifier}+Shift+${cfg.config.left}" = "move left";
-        "${cfg.config.modifier}+Shift+${cfg.config.down}" = "move down";
-        "${cfg.config.modifier}+Shift+${cfg.config.up}" = "move up";
-        "${cfg.config.modifier}+Shift+${cfg.config.right}" = "move right";
-        "${cfg.config.modifier}+Shift+Left" = "move left";
-        "${cfg.config.modifier}+Shift+Down" = "move down";
-        "${cfg.config.modifier}+Shift+Up" = "move up";
-        "${cfg.config.modifier}+Shift+Right" = "move right";
+        seat = { "seat0" = { hide_cursor = "3000"; }; };
 
-        # split workspace
-        "${cfg.config.modifier}+b" = "splith";
-        "${cfg.config.modifier}+v" = "splitv";
+        keybindings = {
+          # start a terminal
+          "${cfg.config.modifier}+Return" = "exec ${cfg.config.terminal}";
 
-        # enter fullscreen mode for the focused container
-        "${cfg.config.modifier}+f" = "fullscreen toggle";
+          # control screen brightness
+          "XF86MonBrightnessUp" = "exec light -A 10";
+          "XF86MonBrightnessDown" = "exec light -U 10";
 
-        # focus the parent container
-        "${cfg.config.modifier}+a" = "focus parent";
+          # control media volume
+          "XF86AudioMute" = "exec pamixer --toggle-mute";
+          "XF86AudioRaiseVolume" = "exec pamixer -i 5";
+          "XF86AudioLowerVolume" = "exec pamixer -d 5";
 
-        # change container layout
-        "${cfg.config.modifier}+s" = "layout stacking";
-        "${cfg.config.modifier}+w" = "layout tabbed";
-        "${cfg.config.modifier}+e" = "layout toggle split";
+          # kill focused window
+          "${cfg.config.modifier}+Shift+q" = "kill";
 
-        # toggle tiling / floating
-        "${cfg.config.modifier}+Shift+space" = "floating toggle";
+          # start a menu app
+          "${cfg.config.modifier}+d" = "exec ${cfg.config.menu}";
 
-        # change focus between tiling / floating windows
-        "${cfg.config.modifier}+space" = "focus mode_toggle";
+          # make a screenshot
+          "${cfg.config.modifier}+Print" = "exec ~/.local/bin/maim.sh";
+          "${cfg.config.modifier}+Shift+Print" = "exec ~/.local/bin/maim.sh -s";
 
-        # focus workspace
-        "${cfg.config.modifier}+1" = "workspace number 1";
-        "${cfg.config.modifier}+2" = "workspace number 2";
-        "${cfg.config.modifier}+3" = "workspace number 3";
-        "${cfg.config.modifier}+4" = "workspace number 4";
-        "${cfg.config.modifier}+5" = "workspace number 5";
-        "${cfg.config.modifier}+6" = "workspace number 6";
-        "${cfg.config.modifier}+7" = "workspace number 7";
-        "${cfg.config.modifier}+8" = "workspace number 8";
-        "${cfg.config.modifier}+9" = "workspace number 9";
+          # change focus
+          "${cfg.config.modifier}+${cfg.config.left}" = "focus left";
+          "${cfg.config.modifier}+${cfg.config.down}" = "focus down";
+          "${cfg.config.modifier}+${cfg.config.up}" = "focus up";
+          "${cfg.config.modifier}+${cfg.config.right}" = "focus right";
+          "${cfg.config.modifier}+Left" = "focus left";
+          "${cfg.config.modifier}+Down" = "focus down";
+          "${cfg.config.modifier}+Up" = "focus up";
+          "${cfg.config.modifier}+Right" = "focus right";
 
-        # move focused container to workspace
-        "${cfg.config.modifier}+Shift+1" =
-          "move container to workspace number 1";
-        "${cfg.config.modifier}+Shift+2" =
-          "move container to workspace number 2";
-        "${cfg.config.modifier}+Shift+3" =
-          "move container to workspace number 3";
-        "${cfg.config.modifier}+Shift+4" =
-          "move container to workspace number 4";
-        "${cfg.config.modifier}+Shift+5" =
-          "move container to workspace number 5";
-        "${cfg.config.modifier}+Shift+6" =
-          "move container to workspace number 6";
-        "${cfg.config.modifier}+Shift+7" =
-          "move container to workspace number 7";
-        "${cfg.config.modifier}+Shift+8" =
-          "move container to workspace number 8";
-        "${cfg.config.modifier}+Shift+9" =
-          "move container to workspace number 9";
+          # move focused window
+          "${cfg.config.modifier}+Shift+${cfg.config.left}" = "move left";
+          "${cfg.config.modifier}+Shift+${cfg.config.down}" = "move down";
+          "${cfg.config.modifier}+Shift+${cfg.config.up}" = "move up";
+          "${cfg.config.modifier}+Shift+${cfg.config.right}" = "move right";
+          "${cfg.config.modifier}+Shift+Left" = "move left";
+          "${cfg.config.modifier}+Shift+Down" = "move down";
+          "${cfg.config.modifier}+Shift+Up" = "move up";
+          "${cfg.config.modifier}+Shift+Right" = "move right";
 
-        # move the currently focused window to the scratchpad
-        "${cfg.config.modifier}+Shift+minus" = "move scratchpad";
-        # cycle through scratchpad windows
-        "${cfg.config.modifier}+minus" = "scratchpad show";
+          # split workspace
+          "${cfg.config.modifier}+b" = "splith";
+          "${cfg.config.modifier}+v" = "splitv";
 
-        "${cfg.config.modifier}+Shift+c" = "reload";
-        "${cfg.config.modifier}+Shift+e" =
-          "exec swaynag -t warning -m 'You pressed the exit shortcut. Do you really want to exit sway? This will end your Wayland session.' -b 'Yes, exit sway' 'swaymsg exit'";
+          # enter fullscreen mode for the focused container
+          "${cfg.config.modifier}+f" = "fullscreen toggle";
 
-        "${cfg.config.modifier}+r" = "mode resize";
-      };
+          # focus the parent container
+          "${cfg.config.modifier}+a" = "focus parent";
 
-      modes = {
-        resize = {
-          "${cfg.config.left}" = "resize shrink width 10 px";
-          "${cfg.config.down}" = "resize grow height 10 px";
-          "${cfg.config.up}" = "resize shrink height 10 px";
-          "${cfg.config.right}" = "resize grow width 10 px";
-          "Left" = "resize shrink width 10 px";
-          "Down" = "resize grow height 10 px";
-          "Up" = "resize shrink height 10 px";
-          "Right" = "resize grow width 10 px";
-          "Escape" = "mode default";
-          "Return" = "mode default";
+          # change container layout
+          "${cfg.config.modifier}+s" = "layout stacking";
+          "${cfg.config.modifier}+w" = "layout tabbed";
+          "${cfg.config.modifier}+e" = "layout toggle split";
+
+          # toggle tiling / floating
+          "${cfg.config.modifier}+Shift+space" = "floating toggle";
+
+          # change focus between tiling / floating windows
+          "${cfg.config.modifier}+space" = "focus mode_toggle";
+
+          # focus workspace
+          "${cfg.config.modifier}+1" = "workspace number 1";
+          "${cfg.config.modifier}+2" = "workspace number 2";
+          "${cfg.config.modifier}+3" = "workspace number 3";
+          "${cfg.config.modifier}+4" = "workspace number 4";
+          "${cfg.config.modifier}+5" = "workspace number 5";
+          "${cfg.config.modifier}+6" = "workspace number 6";
+          "${cfg.config.modifier}+7" = "workspace number 7";
+          "${cfg.config.modifier}+8" = "workspace number 8";
+          "${cfg.config.modifier}+9" = "workspace number 9";
+
+          # move focused container to workspace
+          "${cfg.config.modifier}+Shift+1" =
+            "move container to workspace number 1";
+          "${cfg.config.modifier}+Shift+2" =
+            "move container to workspace number 2";
+          "${cfg.config.modifier}+Shift+3" =
+            "move container to workspace number 3";
+          "${cfg.config.modifier}+Shift+4" =
+            "move container to workspace number 4";
+          "${cfg.config.modifier}+Shift+5" =
+            "move container to workspace number 5";
+          "${cfg.config.modifier}+Shift+6" =
+            "move container to workspace number 6";
+          "${cfg.config.modifier}+Shift+7" =
+            "move container to workspace number 7";
+          "${cfg.config.modifier}+Shift+8" =
+            "move container to workspace number 8";
+          "${cfg.config.modifier}+Shift+9" =
+            "move container to workspace number 9";
+
+          # move the currently focused window to the scratchpad
+          "${cfg.config.modifier}+Shift+minus" = "move scratchpad";
+          # cycle through scratchpad windows
+          "${cfg.config.modifier}+minus" = "scratchpad show";
+
+          "${cfg.config.modifier}+Shift+c" = "reload";
+          "${cfg.config.modifier}+Shift+e" =
+            "exec swaynag -t warning -m 'You pressed the exit shortcut. Do you really want to exit sway? This will end your Wayland session.' -b 'Yes, exit sway' 'swaymsg exit'";
+
+          "${cfg.config.modifier}+r" = "mode resize";
+        };
+
+        modes = {
+          resize = {
+            "${cfg.config.left}" = "resize shrink width 10 px";
+            "${cfg.config.down}" = "resize grow height 10 px";
+            "${cfg.config.up}" = "resize shrink height 10 px";
+            "${cfg.config.right}" = "resize grow width 10 px";
+            "Left" = "resize shrink width 10 px";
+            "Down" = "resize grow height 10 px";
+            "Up" = "resize shrink height 10 px";
+            "Right" = "resize grow width 10 px";
+            "Escape" = "mode default";
+            "Return" = "mode default";
+          };
         };
       };
     };
+
+    home = {
+      file.".local/share/wallpaper.png".source = ./wallpaper.png;
+      packages = with lib;
+        with pkgs;
+        mkIf cfg.enable [
+          # supporting libraries
+          libnotify
+          qt5.qtwayland
+
+          # sway components
+          swaybg # required by sway for controlling desktop wallpaper
+          swayidle # used for controlling idle timeouts and triggers (screen locking, etc)
+          swaylock # used for locking Wayland sessions
+
+          # wayland programs
+          gebaar-libinput # libinput gestures utility
+          grim # screen image capture
+          imv # image viewer
+          xdg-desktop-portal-wlr # xdg-desktop-portal backend for wlroots
+          ydotool # xdotool for wayland
+
+          pamixer # audio mixer
+          light # backlight control
+        ];
+    };
   };
-
-  config.home.packages = with lib;
-    with pkgs;
-    mkIf cfg.enable [
-      # supporting libraries
-      libnotify
-      qt5.qtwayland
-
-      # sway components
-      swaybg # required by sway for controlling desktop wallpaper
-      swayidle # used for controlling idle timeouts and triggers (screen locking, etc)
-      swaylock # used for locking Wayland sessions
-
-      # wayland programs
-      gebaar-libinput # libinput gestures utility
-      grim # screen image capture
-      imv # image viewer
-      xdg-desktop-portal-wlr # xdg-desktop-portal backend for wlroots
-      ydotool # xdotool for wayland
-    ];
 }
