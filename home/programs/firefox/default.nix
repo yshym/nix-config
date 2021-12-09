fc72102d6301d12106fc9d3f090aff6ea0cb6d58
Yevhen Shymotiuk <yevhenshymotiuk@pm.me>
2021-12-09 19:57:43 +0200

    firefox: use firefox-wayland package

--- home/programs/firefox/default.nix
+++ home/programs/firefox/default.nix
@@ -4,6 +4,7 @@
   programs = {
     firefox = {
       enable = true;
+      package = pkgs.firefox-wayland;
       profiles.default = {
         settings = { "browser.search.defaultenginename" = "duckduckgo"; };
         userChrome = builtins.readFile ./userChrome.css;
