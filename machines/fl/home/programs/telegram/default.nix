b10961d6189c026affffeda92535e85408e2549d
Yevhen Shymotiuk <yevhenshymotiuk@pm.me>
2021-12-09 19:49:43 +0200

    fl: add telegram

---
+++ machines/fl/home/programs/telegram/default.nix
@@ -0,0 +1,9 @@
+{ config, lib, pkgs, ... }:
+
+{
+  home = {
+    file.".local/share/TelegramDesktop/tdata/shortcuts-custom.json".source =
+      ./shortcuts-custom.json;
+    packages = [ pkgs.tdesktop ];
+  };
+}
