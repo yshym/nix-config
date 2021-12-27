0d31f058dcfc2300c1181fd92898724b20aaa4fc
Yevhen Shymotiuk <yevhenshymotiuk@pm.me>
2021-12-27 16:40:13 +0200

    emacs: refactor

--- home/programs/emacs/default.nix
+++ home/programs/emacs/default.nix
@@ -9,23 +9,23 @@ let
     (map (n: path + ("/" + n))
       (filter (n: match ".*\\.patch" n != null) (attrNames (readDir path))));
 
-  emacs = withPatches pkgs.emacsPgtk (patches ./patches/emacs27);
-  emacsGit = withPatches pkgs.emacsGit (patches ./patches/emacs28);
   emacsMacport = withPatches pkgs.emacsMacport (patches ./patches/emacsMacport);
-  emacsGcc =
-    ((withPatches pkgs.emacsPgtkGcc (patches ./patches/emacs27)).overrideAttrs
-      (old: {
-        buildInputs = old.buildInputs
-          ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
-            pkgs.darwin.apple_sdk.frameworks.CoreFoundation
-            pkgs.darwin.apple_sdk.frameworks.WebKit
-          ];
+  emacs = with pkgs; if stdenv.isDarwin then emacsMacport else emacsPgtk;
+  emacsGcc = ((if pkgs.stdenv.isDarwin then
+    pkgs.emacsGcc
+  else
+    pkgs.emacsPgtkGcc).overrideAttrs (old:
+      with pkgs; {
+        buildInputs = old.buildInputs ++ lib.optionals stdenv.isDarwin [
+          pkgs.darwin.apple_sdk.frameworks.CoreFoundation
+          pkgs.darwin.apple_sdk.frameworks.WebKit
+        ];
       })).override { withXwidgets = true; };
 in {
   config = mkIf cfg.enable {
     programs = {
       emacs = {
-        package = if pkgs.stdenv.isDarwin then emacsMacport else pkgs.emacsPgtk;
+        package = emacs;
         extraPackages = epkgs: [ epkgs.vterm ];
       };
       zsh = {
