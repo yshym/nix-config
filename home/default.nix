2ca5b86c9766ac4628968efc588a24550be4c7ae
Yevhen Shymotiuk <yevhenshymotiuk@pm.me>
2021-12-27 16:40:39 +0200

    Fix home.stateVersion

--- home/default.nix
+++ home/default.nix
@@ -6,7 +6,7 @@
     useGlobalPkgs = true;
     users.yevhenshymotiuk = { pkgs, ... }: {
       imports = [ ./packages.nix ./programs ];
-      home.stateVersion = "21.05";
+      home.stateVersion = "21.11";
     };
   };
 }
