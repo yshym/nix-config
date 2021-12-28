1156ede6712a6666ba945308603dbd433911ee32
Yevhen Shymotiuk <yevhenshymotiuk@pm.me>
2021-12-28 17:33:04 +0200

    firefox: add passff-host

--- home/programs/firefox/default.nix
+++ home/programs/firefox/default.nix
@@ -1,6 +1,7 @@
-{ pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
-{
+let cfg = config.programs.firefox;
+in {
   programs = {
     firefox = {
       enable = true;
@@ -11,4 +12,8 @@
     };
     zsh.sessionVariables.BROWSER = "firefox";
   };
+
+  home.file.".mozilla/native-messaging-hosts/passff.json".source =
+    lib.mkIf cfg.enable
+    "${pkgs.passff-host}/lib/mozilla/native-messaging-hosts/passff.json";
 }
