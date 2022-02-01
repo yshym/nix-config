bc994b87b58f58462ecd31c302a2bb89ee384411
Yevhen Shymotiuk <yevhenshymotiuk@pm.me>
2022-02-01 23:36:11 +0200

    emacs: pin pdf-tools to NIX_PATH without nixpkgs build fix revision

--- home/programs/emacs/doom.d/packages.el
+++ home/programs/emacs/doom.d/packages.el
@@ -1,18 +1,19 @@
-;; -*- no-byte-compile: t; -*-
-;;; ~/.doom.d/packages.el
+;;; packages.el --- Packages -*- no-byte-compile: t; -*-
+
+;;; Commentary:
+
+;;; Code:
 
 (package! ansi-color)
 (package! atomic-chrome)
 (package! direnv)
 (package! elisp-format)
 (package! org-trello)
-;; TODO Remove when relevant PR is merged
-(unpin! pdf-tools)
-(package! pdf-tools :recipe
-  (:host github
-   :repo "yevhenshymotiuk/pdf-tools"
-   :branch "nixos-without-nix-path"))
+(package! pdf-tools :pin "72ef774320d9eacd4d79b5016e9e989ab176afba")
 (package! poetry)
 (package! protobuf-mode)
 (package! undo-tree)
 (package! verb)
+
+(provide 'packages)
+;;; packages.el ends here
