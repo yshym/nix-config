3fd881ac90073cecf2c31358d6a3516d219ff07c
Yevhen Shymotiuk <yshym@pm.me>
2022-02-06 02:33:46 +0200

    fl: add chromium

--- hosts/fl/home/programs/default.nix
+++ hosts/fl/home/programs/default.nix
@@ -4,6 +4,12 @@ with lib.my; {
   imports = mapModules' ./. import;
 
   programs = {
+    chromium = {
+      enable = true;
+      package = pkgs.ungoogled-chromium.override {
+        commandLineArgs = "--enable-features=UseOzonePlatform --ozone-platform=wayland --force-dark-mode";
+      };
+    };
     emacs.enable = true;
     firefox.package = pkgs.firefox-wayland;
     git.gpgKey = "4B0D9393F36E588A";
