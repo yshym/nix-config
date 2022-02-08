230a1ff1eebf448fbd94d5c25fe9ab47368dda78
Yevhen Shymotiuk <yshym@pm.me>
2022-02-08 22:33:24 +0200

    emacs: unpin pdf-tools

--- home/modules/editors/emacs/doom.d/packages.el
+++ home/modules/editors/emacs/doom.d/packages.el
@@ -9,7 +9,6 @@
 (package! direnv)
 (package! elisp-format)
 (package! org-trello)
-(package! pdf-tools :pin "72ef774320d9eacd4d79b5016e9e989ab176afba")
 (package! poetry)
 (package! protobuf-mode)
 (package! undo-tree)
