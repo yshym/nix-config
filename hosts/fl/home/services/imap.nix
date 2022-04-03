2e16808374390047741c88c37da5953d02a2536c
Yevhen Shymotiuk <yshym@pm.me>
2022-04-03 21:18:04 +0300

    fl: fix protonmail-bridge service exit on system boot

--- hosts/fl/home/services/imap.nix
+++ hosts/fl/home/services/imap.nix
@@ -2,11 +2,17 @@
 
 {
   systemd.user.services = {
-    proton-bridge = {
-      Unit.Description = "proton-bridge";
+    protonmail-bridge = {
+      Unit = {
+        Description = "Protonmail Bridge";
+        After = [ "network.target" ];
+        Before = lib.optional config.accounts.email.accounts.protonmail.imapnotify.enable
+          "imapnotify-protonmail.service";
+      };
       Service = {
+        ExecStartPre = "${pkgs.coreutils}/bin/sleep 5";
         ExecStart =
-          "${pkgs.protonmail-bridge}/bin/protonmail-bridge --no-window";
+          "${pkgs.protonmail-bridge}/bin/protonmail-bridge --no-window --noninteractive";
         Restart = "always";
         RestartSec = 30;
       };
