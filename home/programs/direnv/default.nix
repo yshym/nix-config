8f5481898fef47663bc31aaecfaea9585b7a76bb
Yevhen Shymotiuk <yshym@pm.me>
2022-05-29 02:22:10 +0300

    direnv: add direnvrc

--- home/programs/direnv/default.nix
+++ home/programs/direnv/default.nix
@@ -1,5 +1,6 @@
 { config, lib, pkgs, ... }:
 
+with builtins;
 with lib;
 with pkgs; {
   programs = {
@@ -16,21 +17,7 @@ with pkgs; {
           exact = [ "/etc/nixos" ];
         };
       };
-      stdlib = ''
-        layout_poetry() {
-          if [[ ! -f pyproject.toml ]]; then
-            log_error 'No pyproject.toml found. Use `poetry new` or `poetry init` to create one first.'
-            exit 2
-          fi
-
-          # create venv if it doesn't exist
-          poetry run true
-
-          export VIRTUAL_ENV=$(poetry env info --path)
-          export POETRY_ACTIVE=1
-          PATH_add "$VIRTUAL_ENV/bin"
-        }
-      '';
+      stdlib = readFile ./direnvrc;
     };
     zsh.shellAliases = {
       dhook = ''eval "$(direnv hook zsh)"'';
