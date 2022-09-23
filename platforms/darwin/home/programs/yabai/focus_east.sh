37939460c40210f32db475933b6888d3451b65e6
Yevhen Shymotiuk <yshym@pm.me>
2022-09-24 00:59:05 +0300

    yabai: fix focus_east and focus_west

--- platforms/darwin/home/programs/yabai/focus_east.sh
+++ platforms/darwin/home/programs/yabai/focus_east.sh
@@ -3,6 +3,6 @@
 yabai -m window --focus east \
     || yabai -m window --focus "$( \
         (yabai -m query --spaces --display next || yabai -m query --spaces --display first) \
-            | jq -re '.[] | select(.visible == 1)."first-window"')" \
+            | jq -re '.[] | select(."is-visible" == true)."first-window"')" \
     || yabai -m display --focus next \
     || yabai -m display --focus first
--- platforms/darwin/home/programs/yabai/focus_west.sh
+++ platforms/darwin/home/programs/yabai/focus_west.sh
@@ -3,6 +3,6 @@
 yabai -m window --focus west \
     || yabai -m window --focus "$( \
         (yabai -m query --spaces --display prev || yabai -m query --spaces --display last) \
-            | jq -re '.[] | select(.visible == 1)."last-window"')" \
+            | jq -re '.[] | select(."is-visible" == true)."last-window"')" \
     || yabai -m display --focus prev \
     || yabai -m display --focus last
