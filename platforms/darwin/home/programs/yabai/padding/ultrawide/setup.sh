ef8ae93c481f25646fd6f10e6e5d0ab27257bedc
Yevhen Shymotiuk <yshym@pm.me>
2024-06-13 17:07:53 +0300

    yabai: fix ultrawide padding setup script

--- platforms/darwin/home/programs/yabai/padding/ultrawide/setup.sh
+++ platforms/darwin/home/programs/yabai/padding/ultrawide/setup.sh
@@ -4,19 +4,26 @@
 # Change padding depending on whether an ultrawide display is connected
 #
 
+PADDING=10
 ULTRAWIDE_PADDING=500
 ULTRAWIDE_DISPLAY_UUID="26F43807-614D-4297-BFF2-B35B1D7A6547";
 
 ULTRAWIDE_DISPLAY_ID=$(yabai -m query --displays | jq --arg ULTRAWIDE_DISPLAY_UUID "$ULTRAWIDE_DISPLAY_UUID" '.[] | select(.uuid == $ULTRAWIDE_DISPLAY_UUID) .id')
 DISPLAY_ID=$(yabai -m query --displays | jq ".[0].id")
 
-[ "$DISPLAY_ID" = "$ULTRAWIDE_DISPLAY_ID" ] \
-    && yabai -m config left_padding "$ULTRAWIDE_PADDING" \
-    && yabai -m config right_padding "$ULTRAWIDE_PADDING"
+if [ "$DISPLAY_ID" = "$ULTRAWIDE_DISPLAY_ID" ]; then
+    echo "Ultrawide display detected. Setting padding to $ULTRAWIDE_PADDING"
+    yabai -m config left_padding "$ULTRAWIDE_PADDING" \
+        && yabai -m config right_padding "$ULTRAWIDE_PADDING"
+else
+    echo "No ultrawide display detected. Setting padding to $PADDING"
+    yabai -m config left_padding "$PADDING" \
+        && yabai -m config right_padding "$PADDING"
+fi
 
 yabai -m signal --add event=display_added action='[ "$YABAI_DISPLAY_ID" = "$ULTRAWIDE_DISPLAY_ID" ] \
     && yabai -m config left_padding "$ULTRAWIDE_PADDING" \
     && yabai -m config right_padding "$ULTRAWIDE_PADDING'
 yabai -m signal --add event=display_removed action='[ "$YABAI_DISPLAY_ID" = "$ULTRAWIDE_DISPLAY_ID" ] \
-    && yabai -m config left_padding "$ULTRAWIDE_PADDING" \
-    && yabai -m config right_padding "$ULTRAWIDE_PADDDING"'
+    && yabai -m config left_padding "$PADDING" \
+    && yabai -m config right_padding "$PADDDING"'
