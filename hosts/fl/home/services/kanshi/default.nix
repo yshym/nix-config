0365baa4ce72d224b3c25f9fc7984fe6a886af7d
Yevhen Shymotiuk <yshym@pm.me>
2024-08-02 15:18:03 +0300

    fl: update kanshi config

--- hosts/fl/home/services/kanshi/default.nix
+++ hosts/fl/home/services/kanshi/default.nix
@@ -8,18 +8,18 @@
       docked = {
         outputs = [
           {
-            criteria = "DP-1";
+            criteria = "DP-4";
             position = "0,0";
-            # scale = 1.5;
+            scale = 1.5;
           }
           {
             criteria = "eDP-1";
-            status = "disable";
-            position = "1280,0";
+            # status = "disable";
+            position = "720,960";
           }
         ];
-        # exec =
-        #   [ "${pkgs.sway}/bin/swaymsg workspace 1, move workspace to DP-1" ];
+        exec =
+          [ "${pkgs.sway}/bin/swaymsg workspace 1, move workspace to DP-4" ];
       };
     };
   };
