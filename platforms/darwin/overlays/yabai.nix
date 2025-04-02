30292ea034bfc461d19366adc49e2b9dfe56743d
Yevhen Shymotiuk <yshym@pm.me>
2025-04-02 17:08:03 +0300

    yabai: 7.1.5 -> 7.1.10

--- platforms/darwin/overlays/yabai.nix
+++ platforms/darwin/overlays/yabai.nix
@@ -4,11 +4,11 @@ self: super:
 {
   # NOTE Used to always keep yabai up-to-date
   yabai = super.yabai.overrideAttrs (o: rec {
-    version = "7.1.5";
+    version = "7.1.10";
     src = builtins.fetchTarball {
       url =
         "https://github.com/koekeishiya/yabai/releases/download/v${version}/yabai-v${version}.tar.gz";
-      sha256 = "12yf85cpkviw23ghbz54ayklzxm6n1m10ciy7vrs99g8sm2cgnix";
+      sha256 = "1yags8amblfh6bdll3cirwi4xd7npdfg9y87l3ly5n7pdk4w1nwj";
     };
 
     installPhase = ''
