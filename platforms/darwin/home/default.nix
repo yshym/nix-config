6cfe7e6bc9738267a93ffa412cdaac8349fb9d30
Yevhen Shymotiuk <yshym@pm.me>
2025-04-02 17:09:33 +0300

    darwin: fix application linking

--- platforms/darwin/home/default.nix
+++ platforms/darwin/home/default.nix
@@ -25,7 +25,7 @@ in
         echo "setting up ~/Applications/Nix..."
         hash-app() {
           path="$1/Contents/MacOS"; shift
-          for bin in $(find "$path" -perm +111 -type f -maxdepth 1 2>/dev/null); do
+          find "$path" -perm +111 -type f -maxdepth 3 2>/dev/null | while read -r bin; do
             md5sum "$bin" | cut -b-32
           done | md5sum | cut -b-32
         }
@@ -34,34 +34,32 @@ in
         export APPLICATIONS_DIR="$HOME/Applications/Nix"
         mkdir -p "$APPLICATIONS_DIR"
         export IFS=$'\n'
-        for app in $(find ${config.system.build.applications}/Applications -maxdepth 1 -type l); do
+        find ${config.system.build.applications}/Applications -maxdepth 1 -type l | while read -r app; do
           name="$(basename "$app")"
           src="$(/usr/bin/stat -f%Y "$app")"
           dst="$APPLICATIONS_DIR/$name"
           if [ -h "$src/Contents/MacOS" ]; then
             src_tmp="/tmp/$name"
+            rm -rf "$src_tmp"
             mkdir "$src_tmp"
-            cp -r $src/* "$src_tmp"
+            cp -r "$src"/* "$src_tmp"
             macos_dir="$src_tmp/Contents/MacOS"
-            rm "$macos_dir"
+            rm -rf "$macos_dir"
             mkdir -p "$macos_dir"
             cd "$src/Contents"
-            cp -r $(readlink "MacOS")/* "$macos_dir"
+            cp -r "$(readlink "MacOS")"/* "$macos_dir"
             src="$src_tmp"
           fi
-          hash1="$(hash-app "$src")"
-          hash2="$(hash-app "$dst")"
-          if [ "$hash1" != "$hash2" ]; then
+          if [ "$(hash-app "$src")" != "$(hash-app "$dst")" ]; then
             echo "Current hash of '$name' differs from the Nix store's"
             cp -R "$src" "$APPLICATIONS_DIR"
           fi
-          rm -rf "$src_tmp"
         done
 
         # create Proton Drive symlink
         ln -snf $HOME/Library/CloudStorage/ProtonDrive-yshym@pm.me $HOME/ProtonDrive
 
-        # crate org directory symlink
+        # create org directory symlink
         mkdir -p $HOME/dev
         ln -snf $HOME/ProtonDrive/org $HOME/dev/org
       ''
