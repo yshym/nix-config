da3efcf4ba55ab92c32e5eb3307ace39c9b31c3b
Yevhen Shymotiuk <yshym@pm.me>
2025-12-13 20:23:58 +0200

    darwin: fix application script

--- platforms/darwin/home/default.nix
+++ platforms/darwin/home/default.nix
@@ -1,69 +1,64 @@
 { config, pkgs, ... }:
 
+let
+  applicationsEnv = pkgs.buildEnv {
+    name = "applications";
+    paths = config.environment.systemPackages
+      ++ config.user.packages
+      ++ config.home.home.packages;
+    pathsToLink = [ "/Applications" ];
+  };
+in
 {
   system = {
     primaryUser = config.user.name;
-    build.applications = pkgs.lib.mkForce (pkgs.buildEnv {
-      name = "applications";
-      paths = config.environment.systemPackages
-        ++ config.user.packages
-        ++ config.home.home.packages;
-      pathsToLink = [ "/Applications" ];
-    });
-    activationScripts.applications.text = pkgs.lib.mkForce (
-      ''
-        # Disable the creation of desktop sevice store files
-        defaults write com.apple.desktopservices DSDontWriteNetworkStores true
+    activationScripts.applications.text = pkgs.lib.mkAfter ''
+      # Disable the creation of desktop sevice store files
+      defaults write com.apple.desktopservices DSDontWriteNetworkStores true
 
-        # Copy applications into ~/Applications/Nix. This workaround
-        # allows you to find applications installed by nix through spotlight.
-        echo "setting up ~/Applications/Nix..."
-        hash-app() {
-          path="$1/Contents/MacOS"; shift
-          find "$path" -perm +111 -type f -maxdepth 3 2>/dev/null | while read -r bin; do
-            md5sum "$bin" | cut -b-32
-          done | md5sum | cut -b-32
-        }
-        export USER="yshym"
-        export HOME="/Users/$USER"
-        export APPLICATIONS_DIR="$HOME/Applications/Nix"
-        mkdir -p "$APPLICATIONS_DIR"
-        export IFS=$'\n'
-        find ${config.system.build.applications}/Applications -maxdepth 1 -type l | while read -r app; do
-          name="$(basename "$app")"
-          src="$(/usr/bin/stat -f%Y "$app")"
-          dst="$APPLICATIONS_DIR/$name"
-          if [ -h "$src/Contents/MacOS" ]; then
-            src_tmp="/tmp/$name"
-            rm -rf "$src_tmp"
-            mkdir "$src_tmp"
-            cp -r "$src"/* "$src_tmp"
-            macos_dir="$src_tmp/Contents/MacOS"
-            rm -rf "$macos_dir"
-            mkdir -p "$macos_dir"
-            cd "$src/Contents"
-            cp -r "$(readlink "MacOS")"/* "$macos_dir"
-            src="$src_tmp"
-          fi
-          if [ "$(hash-app "$src")" != "$(hash-app "$dst")" ]; then
-            echo "Current hash of '$name' differs from the Nix store's"
-            cp -R "$src" "$APPLICATIONS_DIR"
-          fi
-        done
+      # Copy applications into ~/Applications/Nix. This workaround
+      # allows you to find applications installed by nix through spotlight.
+      echo "setting up ~/Applications/Nix..."
+      hash-app() {
+        path="$1/Contents/MacOS"; shift
+        find "$path" -perm +111 -type f -maxdepth 3 2>/dev/null | while read -r bin; do
+          md5sum "$bin" | cut -b-32
+        done | md5sum | cut -b-32
+      }
+      export USER="yshym"
+      export HOME="/Users/$USER"
+      export APPLICATIONS_DIR="$HOME/Applications/Nix"
+      mkdir -p "$APPLICATIONS_DIR"
+      export IFS=$'\n'
+      find ${applicationsEnv}/Applications -maxdepth 1 -type l | while read -r app; do
+        name="$(basename "$app")"
+        src="$(/usr/bin/stat -f%Y "$app")"
+        dst="$APPLICATIONS_DIR/$name"
+        if [ -h "$src/Contents/MacOS" ]; then
+          src_tmp="/tmp/$name"
+          rm -rf "$src_tmp"
+          mkdir "$src_tmp"
+          cp -r "$src"/* "$src_tmp"
+          macos_dir="$src_tmp/Contents/MacOS"
+          rm -rf "$macos_dir"
+          mkdir -p "$macos_dir"
+          cd "$src/Contents"
+          cp -r "$(readlink "MacOS")"/* "$macos_dir"
+          src="$src_tmp"
+        fi
+        if [ "$(hash-app "$src")" != "$(hash-app "$dst")" ]; then
+          echo "Current hash of '$name' differs from the Nix store's"
+          cp -R "$src" "$APPLICATIONS_DIR"
+        fi
+      done
 
-        # Create Proton Drive symlink
-        ln -snf $HOME/Library/CloudStorage/ProtonDrive-yshym@pm.me $HOME/ProtonDrive
+      # Create Proton Drive symlink
+      ln -snf $HOME/Library/CloudStorage/ProtonDrive-yshym@pm.me $HOME/ProtonDrive
 
-        # Create org directory symlink
-        mkdir -p $HOME/dev
-        ln -snf $HOME/ProtonDrive/org $HOME/dev/org
-
-        # Create BackgroundMusic driver symlink
-        # NOTE Symlinking doesn't work for some reason
-        # ln -snf ${pkgs.my.BackgroundMusic}/Library/Audio/Plug-Ins/HAL/Background\ Music\ Device.driver /Library/Audio/Plug-Ins/HAL/Background\ Music\ Device.driver
-        # cp -r ${pkgs.my.BackgroundMusic}/Library/Audio/Plug-Ins/HAL/Background\ Music\ Device.driver /Library/Audio/Plug-Ins/HAL/
-      ''
-    );
+      # Create org directory symlink
+      mkdir -p $HOME/dev
+      ln -snf $HOME/ProtonDrive/org $HOME/dev/org
+    '';
   };
 
   home = { pkgs, ... }: { imports = [ ./packages.nix ./programs ]; };
