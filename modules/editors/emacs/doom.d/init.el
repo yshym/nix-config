9f53a8701d5ca393061b4a4df3ff2710627f9625
Yevhen Shymotiuk <yshym@pm.me>
2025-12-16 20:31:48 +0200

    emacs: enable json and janet

--- modules/editors/emacs/doom.d/init.el
+++ modules/editors/emacs/doom.d/init.el
@@ -117,6 +117,8 @@
        ;; (haskell +dante) ; a language that's lazier than I am
        ;; hy               ; readability of scheme w/ speed of python
        ;; idris
+       json              ; At least it ain't XML
+       janet             ; Fun fact: Janet is me!
        ;; (java +meghanada); the poster child for carpal tunnel syndrome
        (javascript +lsp)   ; all(hope(abandon(ye(who(enter(here))))))
        ;; julia            ; a better, faster MATLAB
