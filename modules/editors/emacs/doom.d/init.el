62fdc23af170ac126b4c6855f242a8d616b06ade
Yevhen Shymotiuk <yshym@pm.me>
2026-01-24 23:24:06 +0200

    emacs: disable debugger and emoji

--- modules/editors/emacs/doom.d/init.el
+++ modules/editors/emacs/doom.d/init.el
@@ -23,7 +23,7 @@
        doom                        ; what makes DOOM look the way it does
        doom-dashboard              ; a nifty splash screen for Emacs
        doom-quit                   ; DOOM quit-message prompts when you quit Emacs
-       (emoji +unicode)            ; 🙂
+       ;; (emoji +unicode)            ; 🙂
        ;; fill-column                 ; a `fill-column' indicator
        hl-todo                     ; highlight TODO/FIXME/NOTE tags
        ;; indent-guides               ; highlighted indent columns
@@ -76,7 +76,7 @@
 
        :tools
        ;; ansible
-       debugger                    ; FIXME stepping through code, to help you add bugs
+       ;; debugger                    ; FIXME stepping through code, to help you add bugs
        direnv
        docker
        editorconfig                ; let someone else argue about tabs vs spaces
