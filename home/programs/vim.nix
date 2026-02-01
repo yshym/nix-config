0e2a6dfec732a05a6b17fcf0d7a88ac8c38f01b2
Yevhen Shymotiuk <yshym@pm.me>
2026-02-01 21:13:43 +0200

    vim: update theme config

--- home/programs/vim.nix
+++ home/programs/vim.nix
@@ -6,9 +6,9 @@ let cfg = config.programs.neovim; in
     enable = true;
     withPython3 = true;
     plugins = with pkgs.vimPlugins; [
-      gruvbox
+      lualine-nvim
+      rose-pine
       supertab
-      vim-airline
       vim-airline-themes
       vim-nix
     ];
@@ -20,16 +20,13 @@ let cfg = config.programs.neovim; in
       set shiftwidth=4              " Default indent spacing
       set expandtab                 " Expand [TABS] to spaces
       syntax enable                 " Enable syntax highlighting
-      colorscheme gruvbox           " Set the default colour scheme
+      colorscheme rose-pine         " Set the default colour scheme
       set t_Co=256                  " use 265 colors in vim
       set spell spelllang=en_au     " Default spell checking language
       hi clear SpellBad             " Clear any unwanted default settings
       hi SpellBad cterm=underline   " Set the spell checking highlight style
       hi SpellBad ctermbg=NONE      " Set the spell checking highlight background
       match ErrorMsg '\s\+$'        "
-
-      let g:airline_powerline_fonts = 1   " Use powerline fonts
-      let g:airline_theme = 'gruvbox'     " Set the airline theme
 
       set laststatus=2   " Set up the status line so it's coloured and always on
       inoremap fd <Esc>
