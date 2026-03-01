2758a299f40ce6342c24836d8ccf589e5b4ec0d6
Yevhen Shymotiuk <yshym@pm.me>
2026-03-02 00:02:50 +0200

    darwin: use mac os aliases to link apps

--- platforms/darwin/home/default.nix
+++ platforms/darwin/home/default.nix
@@ -13,43 +13,21 @@ in
   system = {
     primaryUser = config.user.name;
     activationScripts.applications.text = pkgs.lib.mkAfter ''
-      # Disable the creation of desktop sevice store files
+      # Disable the creation of desktop service store files
       defaults write com.apple.desktopservices DSDontWriteNetworkStores true
 
-      # Copy applications into ~/Applications/Nix. This workaround
-      # allows you to find applications installed by nix through spotlight.
+      # Alias applications into ~/Applications/Nix using mkalias
       echo "setting up ~/Applications/Nix..."
-      hash-app() {
-        path="$1/Contents/MacOS"; shift
-        find "$path" -perm +111 -type f -maxdepth 3 2>/dev/null | while read -r bin; do
-          md5sum "$bin" | cut -b-32
-        done | md5sum | cut -b-32
-      }
       export USER="yshym"
       export HOME="/Users/$USER"
       export APPLICATIONS_DIR="$HOME/Applications/Nix"
+      rm -rf "$APPLICATIONS_DIR"
       mkdir -p "$APPLICATIONS_DIR"
-      export IFS=$'\n'
       find ${applicationsEnv}/Applications -maxdepth 1 -type l | while read -r app; do
         name="$(basename "$app")"
         src="$(/usr/bin/stat -f%Y "$app")"
-        dst="$APPLICATIONS_DIR/$name"
-        if [ -h "$src/Contents/MacOS" ]; then
-          src_tmp="/tmp/$name"
-          rm -rf "$src_tmp"
-          mkdir "$src_tmp"
-          cp -r "$src"/* "$src_tmp"
-          macos_dir="$src_tmp/Contents/MacOS"
-          rm -rf "$macos_dir"
-          mkdir -p "$macos_dir"
-          cd "$src/Contents"
-          cp -r "$(readlink "MacOS")"/* "$macos_dir"
-          src="$src_tmp"
-        fi
-        if [ "$(hash-app "$src")" != "$(hash-app "$dst")" ]; then
-          echo "Current hash of '$name' differs from the Nix store's"
-          cp -R "$src" "$APPLICATIONS_DIR"
-        fi
+        echo "aliasing $name" >&2
+        ${pkgs.mkalias}/bin/mkalias "$src" "$APPLICATIONS_DIR/$name"
       done
 
       # Create Proton Drive symlink
