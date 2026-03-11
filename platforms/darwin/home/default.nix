04d4bc0b3c9a50a79cf931a17a4f858be6ede433
Yevhen Shymotiuk <yshym@pm.me>
2026-03-11 13:39:01 +0200

    darwin: use `home.targets.darwin.copyApps`

--- platforms/darwin/home/default.nix
+++ platforms/darwin/home/default.nix
@@ -1,34 +1,11 @@
 { config, pkgs, ... }:
 
-let
-  applicationsEnv = pkgs.buildEnv {
-    name = "applications";
-    paths = config.environment.systemPackages
-      ++ config.user.packages
-      ++ config.home.home.packages;
-    pathsToLink = [ "/Applications" ];
-  };
-in
 {
   system = {
     primaryUser = config.user.name;
     activationScripts.applications.text = pkgs.lib.mkAfter ''
       # Disable the creation of desktop service store files
       defaults write com.apple.desktopservices DSDontWriteNetworkStores true
-
-      # Alias applications into ~/Applications/Nix using mkalias
-      echo "setting up ~/Applications/Nix..."
-      export USER="yshym"
-      export HOME="/Users/$USER"
-      export APPLICATIONS_DIR="$HOME/Applications/Nix"
-      rm -rf "$APPLICATIONS_DIR"
-      mkdir -p "$APPLICATIONS_DIR"
-      find ${applicationsEnv}/Applications -maxdepth 1 -type l | while read -r app; do
-        name="$(basename "$app")"
-        src="$(/usr/bin/stat -f%Y "$app")"
-        echo "aliasing $name" >&2
-        ${pkgs.mkalias}/bin/mkalias "$src" "$APPLICATIONS_DIR/$name"
-      done
 
       # Create Proton Drive symlink
       ln -snf $HOME/Library/CloudStorage/ProtonDrive-yshym@pm.me $HOME/ProtonDrive
@@ -39,5 +16,11 @@ in
     '';
   };
 
-  home = { pkgs, ... }: { imports = [ ./packages.nix ./programs ]; };
+  home = { pkgs, ... }: {
+    imports = [ ./packages.nix ./programs ];
+    targets.darwin.copyApps = {
+      enable = true;
+      directory = "Applications/Nix";
+    };
+  };
 }
