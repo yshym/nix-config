9a3d753889e0cb2bca0072a013e1a115dff14af8
Yevhen Shymotiuk <yshym@pm.me>
2026-04-09 02:29:23 +0300

    yabai: build for arm64e

--- platforms/darwin/overlays/yabai.nix
+++ platforms/darwin/overlays/yabai.nix
@@ -4,13 +4,13 @@ self: super:
 {
   # Used to always keep yabai up-to-date
   yabai = super.yabai.overrideAttrs (o: rec {
-    version = "54728ce89a134d3ff83b356b1f0b86fba50b90b5";
+    version = "v7.1.18";
 
     src = super.fetchFromGitHub {
       owner = "koekeishiya";
       repo = "yabai";
       rev = version;
-      sha256 = "sha256-IVVwlqMlkfkgqmZegVeVwt/YRSIfaH+swTWAikl64wY=";
+      sha256 = "sha256-go3CsFxJCHpEJ8EGv9B5pXt/1AifGLM8S5TIXkhKgDc=";
     };
 
     nativeBuildInputs = o.nativeBuildInputs ++ [ super.xxd ];
@@ -21,11 +21,11 @@ self: super:
     dontBuild = false;
     enableParallelBuilding = false;
 
-    # Strip x86_64 and arm64e cross-compilation flags, but keep arm64
+    # Strip x86_64 and arm64 cross-compilation flags, but keep arm64
     postPatch = ''
       substituteInPlace makefile \
       --replace-fail "-arch x86_64" "" \
-      --replace-fail "-arch arm64e" ""
+      --replace-fail "-arch arm64 " ""
     '';
 
     nativeInstallCheckInputs = [ ];
