786d61701ff90c692c1bed0dcc1caab9fb1d4e91
Yevhen Shymotiuk <yshym@pm.me>
2026-04-13 14:28:14 +0300

    opencode: add caveman skill

---
+++ modules/shell/opencode/default.nix
@@ -0,0 +1,31 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+let cfg = config.modules.shell.git;
+in
+{
+  options.modules.shell.opencode = {
+    enable = mkEnableOption "Opencode";
+  };
+
+  config = mkIf cfg.enable {
+    home = {
+      programs.opencode = {
+        enable = true;
+        package = pkgs.unstable.opencode;
+        settings = {
+          default_agent = "plan";
+          agent.build.permission.edit = "ask";
+          theme = "dracula";
+          autoupdate = false;
+        };
+      };
+      home.file = {
+        ".agents/skills" = {
+          source = ./skills;
+          recursive = true;
+        };
+      };
+    };
+  };
+}
