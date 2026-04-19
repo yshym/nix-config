5c6722af323ba9dfb240397863224fb056f890a9
Yevhen Shymotiuk <yshym@pm.me>
2026-04-20 01:28:37 +0300

    pi: add settings

--- modules/shell/pi/default.nix
+++ modules/shell/pi/default.nix
@@ -1,11 +1,19 @@
 { config, lib, pkgs, ... }:
 
 with lib;
-let cfg = config.modules.shell.pi;
+let
+  cfg = config.modules.shell.pi;
+  settings = {
+    lastChangelogVersion = pkgs.unstable.pi-coding-agent.version;
+    defaultProvider = "openrouter";
+    defaultModel = "anthropic/claude-opus-4.6";
+    terminal = { clearOnShrink = true; };
+  };
 in
 {
   options.modules.shell.pi = {
     enable = mkEnableOption "Pi coding agent";
+    package = mkPackageOption pkgs "unstable.pi-coding-agent" { };
   };
 
   config = mkIf cfg.enable {
@@ -14,6 +22,7 @@ in
       xdg.configFile = {
         "pi/themes/dracula.json".source = ./dracula.json;
         "pi/extensions".source = ./extensions;
+        "pi/settings.json".text = builtins.toJSON settings;
       };
       home.sessionVariables.PI_CODING_AGENT_DIR = "~/.config/pi";
     };
