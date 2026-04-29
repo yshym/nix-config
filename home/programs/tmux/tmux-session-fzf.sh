4ade81b47a5aae0e90248164b37acc0421b831a9
Yevhen Shymotiuk <yshym@pm.me>
2026-04-29 19:17:46 +0300

    tmux-session-fzf: make it smaller

--- home/programs/tmux/tmux-session-fzf.sh
+++ home/programs/tmux/tmux-session-fzf.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-session="$(tmux list-sessions -F '#{session_name}' | fzf-tmux -p 80%,70% \
+session="$(tmux list-sessions -F '#{session_name}' | fzf-tmux -p 50%,50% \
     --no-sort --ansi --border-label 'session' \
-    --preview-window 'right:55%' \
+    --preview-window 'right:50%' \
     --preview 'tmux capture-pane -pt {}')"
 tmux switch -t "$session"
