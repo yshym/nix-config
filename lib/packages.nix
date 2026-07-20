9b4cd204ba8a5482ea7a46bacb1e89b88e31f69a
Yevhen Shymotiuk <yshym@pm.me>
2026-07-20 19:37:51 +0300

    lib: add `mkWrapper`

--- lib/packages.nix
+++ lib/packages.nix
@@ -1,10 +1,23 @@
 { self, lib, pkgs, ... }:
 
 let
-  inherit (lib) foldr intersectLists mapAttrs mapAttrsToList zipAttrs;
+  inherit (lib) foldr intersectLists isList elemAt mapAttrs mapAttrsToList zipAttrs;
   inherit (self.modules) mapModules;
 in
 {
+  # mkWrapper :: package -> postBuild -> derivation
+  #
+  # Wraps a package (or list of packages) with a postBuild script. The first
+  # element of `package` (or `package` itself) names the resulting derivation.
+  mkWrapper = package: postBuild:
+    let name = if isList package then elemAt package 0 else package;
+        paths = if isList package then package else [ package ];
+    in pkgs.symlinkJoin {
+      inherit paths postBuild;
+      name = "${name.pname or name.name or "wrapped"}-wrapped";
+      buildInputs = [ pkgs.makeWrapper ];
+    };
+
   # mapPackages :: [string] -> path -> { ${system} :: { ${name} :: derivation; }; }
   #
   # Builds all packages from .nix files in a directory via callPackage, then
